512e677793024392f70e9230074b75246aef03e7
John Hennelly <jjhennelly@gmail.com>
2014-12-10 01:03:42 -0500

    ContestentApp1

--- angular-apps/angularwithGulp/eLife-gGulpAngular/gulp/build.js
+++ angular-apps/angularwithGulp/eLife-gGulpAngular/gulp/build.js
@@ -92,7 +92,7 @@ gulp.task('html', ['wiredep', 'injector:css', 'injector:js', 'partials'], functi
   var cssFilter = $.filter('**/*.css');
   var assets;
 
-  return gulp.src('src/*.html')
+  return gulp.src('src/index.html')
     .pipe($.inject(gulp.src('.tmp/inject/templateCacheHtml.js', {read: false}), {
       starttag: '<!-- inject:partials -->',
       ignorePath: '.tmp',
